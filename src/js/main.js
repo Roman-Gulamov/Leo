1a5da070f0291a7a4a98a3594e510d2b85e52c11
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-02-22 23:26:20 +0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
-// jQuery(document).ready(function($) {
-//     $("body").css("display", "none");
-// });
+jQuery(document).ready(function($) {
+  $(".header__menu").mouseup(function(event) {
+    let target = $(event.target);
+    if (target.is("p")) {
+        $(this).next().slideToggle().addClass("active");
+        $(this).prev().slideToggle().removeClass("active");
+    }
+  });
+
+  $(document).mouseup(function(event) {
+    let target = $(event.target);
+    if (!target.is("p") && !target.is("a")) {
+      $(".header__menu__list.active").slideUp().removeClass("active");
+      $(".header__menu__list").slideUp().removeClass("active");
+    }
+  });
+
+});
